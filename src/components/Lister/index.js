ef48fddc06ea2d8a2c99e7def22c9c341d306d0e
Bolaji Adeyemi <bolaji.adeyemi@penta.technology>
2021-03-02 15:39:08 +0000

    Implement onSearchTermChange method

--- src/components/Lister/index.js
+++ src/components/Lister/index.js
@@ -3,11 +3,14 @@ import getPosts from '../../services/posts';
 import Post from './Post'
 import CreatePost from './CreatePost';
 import { isEmpty, last } from 'lodash';
+import postData from '../../data/posts/data.json'
+
 
 const Lister = () => {
 
 	const [loading, setLoading] = useState(true);
 	const [allPosts, setPosts] = useState([]);
+	const [searchTerm, setSearchTerm] = useState('');
 
 	useEffect(() => {
 		getPosts().then(data => {
@@ -27,10 +30,27 @@ const Lister = () => {
 
 	}
 
+	const onSearchTermChange = (event) => {
+		setSearchTerm(event.target.value);
+
+		if (isEmpty(event.target.value)) {
+			setPosts(postData);
+		}
+
+		if (searchTerm.length >= 3) {
+			setPosts(allPosts.filter((p) => p.title.toLowerCase().includes(searchTerm.toLowerCase())));
+		}
+
+	}
+
 
 	return (<div className="postList">
 		{loading && <p>Loading...</p>}
 		{isEmpty(allPosts) && <p>No posts available...</p>}
+		<div>
+			<input placeholder="search post by title...." value={searchTerm} onChange={onSearchTermChange} />
+		</div>
+
 		{allPosts.map(({ id, title, body, author }) =>
 			<Post
 				key={id}
@@ -48,3 +68,4 @@ const Lister = () => {
 };
 
 export default Lister;
+
